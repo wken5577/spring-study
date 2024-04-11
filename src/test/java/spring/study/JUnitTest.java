7575ee332a1c898c8bfa5d37b3dc4c68b50c9be1
wken5577 <wken5577@hanmail.net>
2024-04-11 17:11:05 +0900

    Spring을 사용해서 테스트 진행시 항상 같은 ApplicationContext를 사용한다.

--- src/test/java/spring/study/JUnitTest.java
+++ src/test/java/spring/study/JUnitTest.java
@@ -5,27 +5,63 @@ import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@DisplayName("JUnit은 각method마다 새로운 객체를 만들어서 테스트를 진행한다.")
 public class JUnitTest {
-	static Set<JUnitTest> testObjects = new HashSet<>();
+	@Nested
+	@DisplayName("JUnit은 각method마다 새로운 객체를 만들어서 테스트를 진행한다.")
+	class JUnit{
+		static Set<JUnit> testObjects = new HashSet<>();
 
-	@Test
-	public void test1() {
-		Assertions.assertFalse(testObjects.contains(this));
-		testObjects.add(this);
+		@Test
+		public void test1() {
+			Assertions.assertFalse(testObjects.contains(this));
+			testObjects.add(this);
+		}
+
+		@Test
+		public void test2() {
+			Assertions.assertFalse(testObjects.contains(this));
+			testObjects.add(this);
+		}
+
+		@Test
+		public void test3() {
+			Assertions.assertFalse(testObjects.contains(this));
+			testObjects.add(this);
+		}
 	}
 
-	@Test
-	public void test2() {
-		Assertions.assertFalse(testObjects.contains(this));
-		testObjects.add(this);
-	}
+	@Nested
+	@DisplayName("Spring을 사용해서 테스트 진행시 항상 같은 ApplicationContext를 사용한다.")
+	@SpringBootTest
+	class SpringTest{
+		static ApplicationContext context = null;
 
-	@Test
-	public void test3() {
-		Assertions.assertFalse(testObjects.contains(this));
-		testObjects.add(this);
+		@Autowired
+		ApplicationContext applicationContext;
+
+		@Test
+		public void test1() {
+			Assertions.assertTrue(context == null || context == applicationContext);
+			context = applicationContext;
+		}
+
+		@Test
+		public void test2() {
+			Assertions.assertTrue(context == null || context == applicationContext);
+			context = applicationContext;
+		}
+
+		@Test
+		public void test3() {
+			Assertions.assertTrue(context == null || context == applicationContext);
+			context = applicationContext;
+		}
+
 	}
 }
