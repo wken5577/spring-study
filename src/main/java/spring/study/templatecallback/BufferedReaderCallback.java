9e20bf1a415bea34d0cacf218cc1a49b5604b1ba
wken5577 <wken5577@hanmail.net>
2024-04-12 19:59:39 +0900

    template callback pattern

---
+++ src/main/java/spring/study/templatecallback/BufferedReaderCallback.java
@@ -0,0 +1,7 @@
+package spring.study.templatecallback;
+
+import java.io.BufferedReader;
+
+public interface BufferedReaderCallback {
+	Integer doSomethingWithReader(BufferedReader br);
+}
---
+++ src/main/java/spring/study/templatecallback/LineCallback.java
@@ -0,0 +1,5 @@
+package spring.study.templatecallback;
+
+public interface LineCallback<T> {
+	T doSomethingWithLine(String line, T value);
+}
